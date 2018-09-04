09fab312288244b76e113da5b3024dc7d911d850
Shakir Shakiel <shakirshakiel@gmail.com>
2018-09-04 23:52:24 +0530

    Add project_id task and analyse_merge_requests task

--- Rakefile
+++ Rakefile
@@ -9,16 +9,21 @@ Gitlab.configure do |config|
   config.private_token  = yaml["private_token"]
 end
 
-desc "Show all dangling networks"
-task :merge_requests do
+desc "Get project id"
+task :project_id do
   options = {
       project_namespace: ENV['project_namespace'],
       project_search: ENV['project']
   }
-
-  p GitlabAnalyser.analyse_merge_requests(options)
+  p GitlabAnalyser.project_id options
 end
 
+desc "Analyse merge requests of a project"
+task :analyse_merge_requests do
+  p GitlabAnalyser.analyse_merge_requests(3818)
+end
+
+desc "Average response time of a merge request"
 task :average_response_time do
   p GitlabAnalyser.average_response_time_in_merge_request(3818, 165)
 end
--- lib/gitlab_analyser.rb
+++ lib/gitlab_analyser.rb
@@ -5,24 +5,28 @@ require 'pry'
 class GitlabAnalyser
 
   class << self
-    def analyse_merge_requests(options)
+
+    def project_id(options)
       projects = Gitlab.project_search(options[:project_search])
       project = projects.find {|p| p.path_with_namespace == options[:project_namespace]}
-      merge_requests = Gitlab.merge_requests(project.id, {per_page: 100})
-      merge_request_ids = merge_requests.map {|mr| mr.iid}
-      merge_request_ids.each do |mr_id|
-        notes = Gitlab.merge_request_notes project.id, mr_id
-        times = notes.map &:created_at
-        response_times = 0.upto(times.count - 2).map do |t|
-          Time.parse(times[t]) - Time.parse(times[t + 1])
-        end
-        response_times.sum / response_times.size
+      project.id
+    end
+
+    def analyse_merge_requests(project_id)
+      merge_requests = Gitlab.merge_requests(project_id, {per_page: 100})
+      merge_requests.each do |mr|
+        p mr.title
+        p average_response_time_in_merge_request(project_id, mr.iid)
       end
     end
 
     def average_response_time_in_merge_request(project_id, merge_request_id)
+      merge_request = Gitlab.merge_request project_id, merge_request_id
       notes = Gitlab.merge_request_notes project_id, merge_request_id
+      
       times = notes.map &:created_at
+      times.push(merge_request.created_at)
+
       response_times = 0.upto(times.count - 2).map do |t|
         Time.parse(times[t]) - Time.parse(times[t + 1])
       end
